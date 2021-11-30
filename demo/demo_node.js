8a76b5013f6063e0c42be241d61de28bf72a8717
Ray <inthinkcolor@gmail.com>
2021-11-30 14:27:07 +0800

    Code maintenance
    + added codeql-analysis
    # fix tar vulnerability
    # fix fs error in demo code

--- demo/demo_node.js
+++ demo/demo_node.js
@@ -1,164 +1,164 @@
 const QRCode = require('../index.min');
 
-
+const fs = require('fs');
 
 // ================================ PNG Configs
 
 
-var config={
-	// ====== Basic
-	text: "www.easyproject.cn/donation",
-	width: 256,
-	height: 256,
-	quietZone: 0,
-	colorDark: "#000000",
-	colorLight: "#ffffff",
-	correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
-	dotScale: 1 // Must be greater than 0, less than or equal to 1. default is 1
+var config = {
+    // ====== Basic
+    text: "www.easyproject.cn/donation",
+    width: 256,
+    height: 256,
+    quietZone: 0,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
+    dotScale: 1 // Must be greater than 0, less than or equal to 1. default is 1
 }
 
-var config2={
-	// ====== Basic
-	text: "www.easyproject.cn/donation",
-	
-	width: 400,
-	height: 400,
-	quietZone: 0,
-	correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
-	dotScale: 0.5 ,// Must be greater than 0, less than or equal to 1. default is 1
-	colorDark: "#473C8B",
-	colorLight: "#FFFACD",
+var config2 = {
+    // ====== Basic
+    text: "www.easyproject.cn/donation",
 
-	// === Posotion Pattern(Eye) Color
-	PI: '#BF3030',
-	PO: '#269926', 
-	
-	PI_TL: '#b7d28d', // Position Inner - Top Left 
-	PO_TL: '#aa5b71', // Position Outer - Top Right
-	AO: '#336699',  // Position Outer - Bottom Right
-	AI: '#336699',  // Position Inner - Bottom Right
-	
-	// === Aligment color
-	AI: '#009ACD',
-	AO: '#B03060',
-	
-	// === Timing Pattern Color
-	//	timing: '#e1622f', // Global Timing color. if not set, the defaut is `colorDark`
-	timing_H: '#ff6600', // Horizontal timing color
-	timing_V: '#cc0033',// Vertical timing color
-	
-	// === Background image
-	backgroundImage: 'logo.png',
-	backgroundImageAlpha: 0.3,
-	autoColor: true
+    width: 400,
+    height: 400,
+    quietZone: 0,
+    correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
+    dotScale: 0.5, // Must be greater than 0, less than or equal to 1. default is 1
+    colorDark: "#473C8B",
+    colorLight: "#FFFACD",
+
+    // === Posotion Pattern(Eye) Color
+    PI: '#BF3030',
+    PO: '#269926',
+
+    PI_TL: '#b7d28d', // Position Inner - Top Left 
+    PO_TL: '#aa5b71', // Position Outer - Top Right
+    AO: '#336699', // Position Outer - Bottom Right
+    AI: '#336699', // Position Inner - Bottom Right
+
+    // === Aligment color
+    AI: '#009ACD',
+    AO: '#B03060',
+
+    // === Timing Pattern Color
+    //	timing: '#e1622f', // Global Timing color. if not set, the defaut is `colorDark`
+    timing_H: '#ff6600', // Horizontal timing color
+    timing_V: '#cc0033', // Vertical timing color
+
+    // === Background image
+    backgroundImage: 'logo.png',
+    backgroundImageAlpha: 0.3,
+    autoColor: true
 }
 
-var config3={
-	// ====== Basic
-	text: "www.easyproject.cn/donation",
-	
-	width: 400,
-	height: 400,
-	correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
-	dotScale: 0.5 ,// Must be greater than 0, less than or equal to 1. default is 1
-	colorDark: "#473C8B",
-	colorLight: "#FFFACD",
-    
+var config3 = {
+    // ====== Basic
+    text: "www.easyproject.cn/donation",
+
+    width: 400,
+    height: 400,
+    correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
+    dotScale: 0.5, // Must be greater than 0, less than or equal to 1. default is 1
+    colorDark: "#473C8B",
+    colorLight: "#FFFACD",
+
     // QuietZone
-    quietZone:15,
+    quietZone: 15,
 
-	// === Title
-	title: 'EasyQRCode', // Title
-	titleFont: "normal normal bold 24px Arial", // Title font
-	titleColor: "#004284", // Title Color
-	titleBackgroundColor: "#ffffff", // Title Background
-	titleHeight: 70, // Title height, include subTitle
-	titleTop: 25, // Title draw position(Y coordinate), default is 30
-	
-	// === SubTitle
-	subTitle: 'nodejs', // Subtitle content
-	subTitleFont: "normal normal normal 20px Arial", // Subtitle font
-	subTitleColor: "#269926", // Subtitle color
-	subTitleTop: 50, // Subtitle drwa position(Y coordinate), default is 50
-	
-	// === Posotion Pattern(Eye) Color
-	PI: '#BF3030',
-	PO: '#269926', 
-	
-	PI_TL: '#b7d28d', // Position Inner - Top Left 
-	PO_TL: '#aa5b71', // Position Outer - Top Right
-	AO: '#336699',  // Position Outer - Bottom Right
-	AI: '#336699',  // Position Inner - Bottom Right
-	
-	// === Aligment color
-	AI: '#009ACD',
-	AO: '#B03060',
-	
-	// === Timing Pattern Color
-	//	timing: '#e1622f', // Global Timing color. if not set, the defaut is `colorDark`
-	timing_H: '#ff6600', // Horizontal timing color
-	timing_V: '#cc0033' ,// Vertical timing color
-	
-	// === Logo
-	// logo: "https://avatars1.githubusercontent.com/u/4082017?s=160&v=4", // LOGO
-	logo: "avatars.png", // LOGO
-	//					logo:"http://127.0.0.1:8020/easy-qrcodejs/demo/logo.png",  
-	//					logoWidth:80, 
-	//					logoHeight:80,
-	logoBackgroundColor: '#FFF8DC', // Logo backgroud color, Invalid when `logBgTransparent` is true; default is '#ffffff'
-	logoBackgroundTransparent: false, // Whether use transparent image, default is false
-	
-	// === Background image
-	backgroundImage: 'logo.png',
-	backgroundImageAlpha: 0.3,
-	autoColor: true,
-	
-	onRenderingStart:function(options){
-		console.info("The QRCode file `q3."+options.format+"` was created.");
-	}
+    // === Title
+    title: 'EasyQRCode', // Title
+    titleFont: "normal normal bold 24px Arial", // Title font
+    titleColor: "#004284", // Title Color
+    titleBackgroundColor: "#ffffff", // Title Background
+    titleHeight: 70, // Title height, include subTitle
+    titleTop: 25, // Title draw position(Y coordinate), default is 30
+
+    // === SubTitle
+    subTitle: 'nodejs', // Subtitle content
+    subTitleFont: "normal normal normal 20px Arial", // Subtitle font
+    subTitleColor: "#269926", // Subtitle color
+    subTitleTop: 50, // Subtitle drwa position(Y coordinate), default is 50
+
+    // === Posotion Pattern(Eye) Color
+    PI: '#BF3030',
+    PO: '#269926',
+
+    PI_TL: '#b7d28d', // Position Inner - Top Left 
+    PO_TL: '#aa5b71', // Position Outer - Top Right
+    AO: '#336699', // Position Outer - Bottom Right
+    AI: '#336699', // Position Inner - Bottom Right
+
+    // === Aligment color
+    AI: '#009ACD',
+    AO: '#B03060',
+
+    // === Timing Pattern Color
+    //	timing: '#e1622f', // Global Timing color. if not set, the defaut is `colorDark`
+    timing_H: '#ff6600', // Horizontal timing color
+    timing_V: '#cc0033', // Vertical timing color
+
+    // === Logo
+    // logo: "https://avatars1.githubusercontent.com/u/4082017?s=160&v=4", // LOGO
+    logo: "avatars.png", // LOGO
+    //					logo:"http://127.0.0.1:8020/easy-qrcodejs/demo/logo.png",  
+    //					logoWidth:80, 
+    //					logoHeight:80,
+    logoBackgroundColor: '#FFF8DC', // Logo backgroud color, Invalid when `logBgTransparent` is true; default is '#ffffff'
+    logoBackgroundTransparent: false, // Whether use transparent image, default is false
+
+    // === Background image
+    backgroundImage: 'logo.png',
+    backgroundImageAlpha: 0.3,
+    autoColor: true,
+
+    onRenderingStart: function(options) {
+        console.info("The QRCode file 'q3.${options.format}' was created.");
+    }
 }
 
-var config4={
-	// ====== Basic
-	text: "www.easyproject.cn/donation",
-	
-	width: 400,
-	height: 400,
-	correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
-	dotScale: 0.5 ,// Must be greater than 0, less than or equal to 1. default is 1
-	colorDark: "#473C8B",
-	colorLight: "#FFFACD",
-	
-	// QuietZone
-	quietZone:15,
-	quietZoneColor:'#00CED1',
-	
-	// === Posotion Pattern(Eye) Color
-	PI: '#BF3030',
-	PO: '#269926', 
-	
-	PI_TL: '#b7d28d', // Position Inner - Top Left 
-	PO_TL: '#aa5b71', // Position Outer - Top Right
-	AO: '#336699',  // Position Outer - Bottom Right
-	AI: '#336699',  // Position Inner - Bottom Right
-	
-	// === Aligment color
-	AI: '#009ACD',
-	AO: '#B03060',
-	
-	// === Timing Pattern Color
-	//	timing: '#e1622f', // Global Timing color. if not set, the defaut is `colorDark`
-	timing_H: '#ff6600', // Horizontal timing color
-	timing_V: '#cc0033' ,// Vertical timing color
-	
-	// === Logo
-	// logo: "https://avatars1.githubusercontent.com/u/4082017?s=160&v=4", // LOGO
-	logo: "./avatars.png", // LOGO
-	//					logo:"http://127.0.0.1:8020/easy-qrcodejs/demo/logo.png",  
-	//					logoWidth:80, 
-	//					logoHeight:80,
-	logoBackgroundColor: '#FFF8DC', // Logo backgroud color, Invalid when `logBgTransparent` is true; default is '#ffffff'
-	logoBackgroundTransparent: false // Whether use transparent image, default is false
+var config4 = {
+    // ====== Basic
+    text: "www.easyproject.cn/donation",
+
+    width: 400,
+    height: 400,
+    correctLevel: QRCode.CorrectLevel.H, // L, M, Q, H
+    dotScale: 0.5, // Must be greater than 0, less than or equal to 1. default is 1
+    colorDark: "#473C8B",
+    colorLight: "#FFFACD",
+
+    // QuietZone
+    quietZone: 15,
+    quietZoneColor: '#00CED1',
+
+    // === Posotion Pattern(Eye) Color
+    PI: '#BF3030',
+    PO: '#269926',
+
+    PI_TL: '#b7d28d', // Position Inner - Top Left 
+    PO_TL: '#aa5b71', // Position Outer - Top Right
+    AO: '#336699', // Position Outer - Bottom Right
+    AI: '#336699', // Position Inner - Bottom Right
+
+    // === Aligment color
+    AI: '#009ACD',
+    AO: '#B03060',
+
+    // === Timing Pattern Color
+    //	timing: '#e1622f', // Global Timing color. if not set, the defaut is `colorDark`
+    timing_H: '#ff6600', // Horizontal timing color
+    timing_V: '#cc0033', // Vertical timing color
+
+    // === Logo
+    // logo: "https://avatars1.githubusercontent.com/u/4082017?s=160&v=4", // LOGO
+    logo: "./avatars.png", // LOGO
+    //					logo:"http://127.0.0.1:8020/easy-qrcodejs/demo/logo.png",  
+    //					logoWidth:80, 
+    //					logoHeight:80,
+    logoBackgroundColor: '#FFF8DC', // Logo backgroud color, Invalid when `logBgTransparent` is true; default is '#ffffff'
+    logoBackgroundTransparent: false // Whether use transparent image, default is false
 }
 
 
@@ -170,37 +170,37 @@ var qrcode3 = new QRCode(config3);
 var qrcode4 = new QRCode(config4);
 
 qrcode.saveImage({
-	path: 'q.png'
+    path: 'q.png'
 });
 qrcode2.saveImage({
-	path: 'q2.png'
+    path: 'q2.png'
 });
 qrcode3.saveImage({
-	path: 'q3.png'
+    path: 'q3.png'
 });
 qrcode4.saveImage({
-	path: 'q4.png'
+    path: 'q4.png'
 });
-qrcode.toDataURL().then(data=>{
-	console.info('======QRCode PNG DataURL======')
-	console.info(data)
-	console.info('')
+qrcode.toDataURL().then(data => {
+    console.info('======QRCode PNG DataURL======')
+    console.info(data)
+    console.info('')
 });
 
 // ================================ JPG Test
 
 var config5 = Object.assign({}, config, {
-    format:'JPG',
+    format: 'JPG',
     version: 6
 });
 var config6 = Object.assign({}, config2, {
-    format:'JPG'
+    format: 'JPG'
 });
 var config7 = Object.assign({}, config3, {
-    format:'JPG'
+    format: 'JPG'
 });
 var config8 = Object.assign({}, config4, {
-    format:'JPG'
+    format: 'JPG'
 });
 
 var qrcode5 = new QRCode(config5);
@@ -210,22 +210,22 @@ var qrcode8 = new QRCode(config8);
 
 
 qrcode5.saveImage({
-	path: 'q.jpg'
+    path: 'q.jpg'
 });
 qrcode6.saveImage({
-	path: 'q2.jpg'
+    path: 'q2.jpg'
 });
 qrcode7.saveImage({
-	path: 'q3.jpg'
+    path: 'q3.jpg'
 });
 qrcode8.saveImage({
-	path: 'q4.jpg'
+    path: 'q4.jpg'
 });
 
-qrcode5.toSVGText().then(data=>{
-	console.info('======QRCode SVG Data Text======')
-	console.info(data)
-	console.info('')
+qrcode5.toSVGText().then(data => {
+    console.info('======QRCode SVG Data Text======')
+    console.info(data)
+    console.info('')
 });
 
 qrcode8.saveSVG({
@@ -253,7 +253,7 @@ async function generate() {
     // stream.pipe(out);
     // out.on('finish', () => console.log('Finsihed'));
 
-    streamQrcode.toStream().then(res=>{
+    streamQrcode.toStream().then(res => {
         res.pipe(out).on('finish', () => console.log('Stream Finsihed'));
     })
 
